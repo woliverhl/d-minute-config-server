3f329364e4220229ba8dc2583c5d719979b8d1f6
woliverhl <ohidalgoleal@gmail.com>
2018-06-14 21:29:12 -0400

    versión inciial config

---
+++ src/main/java/cl/usach/dminuteconfigserver/DMinuteConfigServerApplication.java
@@ -0,0 +1,12 @@
+package cl.usach.dminuteconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DMinuteConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DMinuteConfigServerApplication.class, args);
+	}
+}
